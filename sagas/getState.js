66b5856bf9c0201f6fb758a1cbef46cd9b31a03c
b8208241 <b8208241@gmail.com>
2017-02-22 15:22:21 +0800

    Delete getState.js

--- sagas/getState.js
+++
@@ -1,3 +0,0 @@
-export const getTopicData = (state) => state.topicData
-export const getTopicThis = (state, topicId) => state.topicData[topicId]
-export const getMemoRecords = (state, topicId) => state.topicData[topicId].memoRecords
